cef539a33f3de40cbc7e9c2d4ec271b93da2b9f9
giniguy <giniguy@u.boisestate.edu>
2016-12-05 06:05:48 -0700

    Delete essentialcontent.php

--- essentialcontent.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-<html>
-	<ul class="tabmenu">
-	  <li><a href="#">Focus on Grade Level Advancement</a></li>
-	  <li><a href="#">Alignment of Activities</a></li>
-	  <li><a href="#">Instructional Materials</a></li>
-	</ul>
